cf64d5ddf9c37de41052903ab0fc320e2cdf4416
jjavier-bm <jjbm.research@gmail.com>
2020-07-30 17:40:40 +0100

    Revert "remove mutable from initialisation"
    This reverts commit 8182e23d207e0f36e12300da61f69095ff2bc68a.

--- crops/core/sequence.py
+++ crops/core/sequence.py
@@ -266,8 +266,8 @@ class monomer_sequence:
     def length(self):
         """Returns the length of the main sequence.
 
-        :return: Length of the main sequence.
-        :rtype: int
+        :return n: Length of the main sequence.
+        :rtype n: int
 
         """
 
@@ -381,18 +381,18 @@ class Sequence:
     """
     kind='Protein/polynucleotide sequence'
     __slots__ = ['seq_id', 'imer', 'source']
-    def __init__(self, seq_id=None, imer=None, source=None):
+    def __init__(self, seq_id=None, imer={}, source=None):
 
         if not isinstance(seq_id,str) and seq_id is not None:
             raise TypeError("Sequence ID 'seq_id' should be a string.")
-        if not isinstance(imer,dict) and imer is not None:
+        if not isinstance(imer,dict):
             raise TypeError("Monomer container 'imer' should be a dictionary.")
-        elif isinstance(imer,dict):
+        else:
             for val in imer.values():
                 if not isinstance(val,monomer_sequence):
                     raise TypeError("Monomer container 'imer' should only contain elements of monomer_sequence class.")
         self.seq_id = seq_id
-        self.imer = {} if imer is None else imer
+        self.imer = imer#.copy()
         self.source = source
 
     def __repr__(self):
