549f228d4d6d9493dc8d90af7ea11433d5ff5674
jjavier-bm <jjbm.research@gmail.com>
2021-02-12 16:16:14 +0000

    loggings added

--- crops/__init__.py
+++ crops/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""This is CROPS: Cropping and Renumbering Operations for PDB structure and Sequence files"""
 
 from crops.about import __prog__, __description__, __author__, __date__, __version__
